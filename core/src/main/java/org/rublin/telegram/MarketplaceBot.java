d3aee60dad7e9f46ae805cb5ffc420e68f0b948b
rublin <toor.ua@gmail.com>
2017-09-17 17:17:11 +0300

    add keyboard

--- core/src/main/java/org/rublin/telegram/MarketplaceBot.java
+++ core/src/main/java/org/rublin/telegram/MarketplaceBot.java
@@ -1,16 +1,27 @@
 package org.rublin.telegram;
 
 import lombok.extern.slf4j.Slf4j;
+import org.rublin.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class MarketplaceBot extends TelegramLongPollingBot {
 
     private String username;
     private String token;
+
+    @Autowired
+    private OrderService orderService;
 
     public MarketplaceBot(String username, String token) {
         this.username = username;
@@ -21,10 +32,49 @@ public class MarketplaceBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
 
         if (update.hasMessage() && update.getMessage().hasText()) {
-//            log.debug("Telegram bot received message {} from {}", update.getMessage().getText(), update.getMessage().getContact().getUserID());
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            SendMessage message = new SendMessage().setChatId(chat_id);
+            log.debug("Telegram bot received message {} from {}", update.getMessage().getText(), update.getMessage().getContact());
+            if (message_text.equals("/start")) {
+                //ToDo
+
+            } else if (message_text.startsWith("/markup")) {
+
+                message.setText("Keyboard");
+                // Create ReplyKeyboardMarkup object
+                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+                // Create the keyboard (list of keyboard rows)
+                List<KeyboardRow> keyboard = new ArrayList<>();
+                // Create a keyboard row
+                KeyboardRow row = new KeyboardRow();
+                // Set each button, you can also use KeyboardButton objects if you need something else than text
+                row.add("Karbo price");
+                row.add("Row 1 Button 2");
+                row.add("Row 1 Button 3");
+                // Add the first row to the keyboard
+                keyboard.add(row);
+                // Create another keyboard row
+                row = new KeyboardRow();
+                // Set each button for the second line
+                row.add("Row 2 Button 1");
+                row.add("Row 2 Button 2");
+                row.add("Row 2 Button 3");
+                // Add the second row to the keyboard
+                keyboard.add(row);
+                // Set the keyboard to the markup
+                keyboardMarkup.setKeyboard(keyboard);
+                // Add it to the message
+                message.setReplyMarkup(keyboardMarkup);
+            } else if (message_text.equals("Karbo price")) {
+                message.setText("Karbo price is ToTheMoon");
+
+            } else {
+                message = new SendMessage() // Create a SendMessage object with mandatory fields
+                        .setChatId(update.getMessage().getChatId())
+                        .setText("Unknown command");
+            }
+
             try {
                 sendMessage(message); // Call method to send the message
             } catch (TelegramApiException e) {
