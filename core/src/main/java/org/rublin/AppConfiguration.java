13f7f2cefea5b2b94d6e53590ea0ec40f93e71b9
Ruslan Sheremet <ruslan.sheremet@dimetogo.com>
2017-09-27 10:13:49 +0300

    move telegram properties to app prop

--- core/src/main/java/org/rublin/AppConfiguration.java
+++ core/src/main/java/org/rublin/AppConfiguration.java
@@ -1,14 +1,9 @@
 package org.rublin;
 
-import org.rublin.service.OrderService;
-import org.rublin.service.OrderServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:telegram.properties")
+//@PropertySource("classpath:telegram.properties")
 public class AppConfiguration {
 
 //    @Bean
