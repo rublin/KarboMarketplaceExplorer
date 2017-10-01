d1ca119efb00a4274929ead926a7c32b40054e13
rublin <toor.ua@gmail.com>
2017-10-01 09:37:22 +0300

    add test sort

--- src/test/org/rublin/service/OrderServiceImplTest.java
+++ src/test/org/rublin/service/OrderServiceImplTest.java
@@ -5,9 +5,12 @@ import org.junit.runner.RunWith;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.rublin.Currency;
+import org.rublin.dto.OptimalOrderDto;
 import org.rublin.dto.PairDto;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -18,17 +21,41 @@ public class OrderServiceImplTest {
 
     @Test
     public void sortOrders() throws Exception {
+        List<OptimalOrderDto> orders = buildOptimalOrders();
+        service.sortOrders(orders, buildPair(true));
+        assertTrue(orders.get(0).getRate().compareTo(orders.get(1).getRate()) < 0);
 
+        service.sortOrders(orders, buildPair(false));
+        assertTrue(orders.get(0).getRate().compareTo(orders.get(1).getRate()) > 0);
     }
 
     @Test
     public void rate() throws Exception {
-        PairDto bought = PairDto.builder().buyCurrency(Currency.KRB).sellCurrency(Currency.BTC).build();
-        PairDto sale = PairDto.builder().sellCurrency(Currency.KRB).buyCurrency(Currency.BTC).build();
+        PairDto bought = buildPair(true);
+        PairDto sale = buildPair(false);
         BigDecimal rate = service.rate(BigDecimal.valueOf(0.00100).setScale(5), BigDecimal.valueOf(10), bought);
         assertEquals(BigDecimal.valueOf(0.0001).stripTrailingZeros(), rate);
         rate = service.rate(BigDecimal.valueOf(10), BigDecimal.valueOf(0.001).setScale(5), sale);
         assertEquals(BigDecimal.valueOf(0.0001).stripTrailingZeros(), rate);
     }
+    
+    private PairDto buildPair(boolean bought) {
+        if (bought) {
+            return PairDto.builder().buyCurrency(Currency.KRB).sellCurrency(Currency.BTC).build();
+        }
+        return PairDto.builder().sellCurrency(Currency.KRB).buyCurrency(Currency.BTC).build();
+    }
+    
+    private List<OptimalOrderDto> buildOptimalOrders() {
+        List<OptimalOrderDto> result = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            result.add(OptimalOrderDto.builder().rate(random()).build());
+        }
 
+        return result;
+    }
+
+    private BigDecimal random() {
+        return BigDecimal.valueOf(Math.random());
+    }
 }
