0cb67d67a3b176dad1575c4976ef8f588d68429a
Ruslan Sheremet <ruslan.sheremet@dimetogo.com>
2017-11-07 13:50:52 +0200

    fix completable future

--- src/main/java/org/rublin/provider/MarketplaceImpl.java
+++ src/main/java/org/rublin/provider/MarketplaceImpl.java
@@ -7,19 +7,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component("marketplace")
 public class MarketplaceImpl implements Marketplace {
 
+    private static final long TIMEOUT_SECONDS = 5;
     @Autowired
     @Qualifier("btc")
     private Marketplace btcTradeMarketplace;
@@ -36,46 +40,31 @@ public class MarketplaceImpl implements Marketplace {
     public List<OptimalOrderDto> tradesByPair(PairDto pair) {
         List<Marketplace> marketplaces = Arrays.asList(cryptopiaMarketplace, livecoinMarketplace, btcTradeMarketplace);
         ExecutorService executorService = Executors.newFixedThreadPool(3);
-        CompletableFuture<List<OptimalOrderDto>> exceptionally1 = CompletableFuture.supplyAsync(() -> btcTradeMarketplace.tradesByPair(pair), executorService)
-                .exceptionally(error -> {
-                    log.error("Marketplace error with {} pair", pair);
-                    return Collections.emptyList();
-                });
-        CompletableFuture<List<OptimalOrderDto>> exceptionally2 = CompletableFuture.supplyAsync(() -> cryptopiaMarketplace.tradesByPair(pair), executorService)
-                .exceptionally(error -> {
-                    log.error("Marketplace error with {} pair", pair);
-                    return Collections.emptyList();
-                });
-        CompletableFuture<List<OptimalOrderDto>> exceptionally3 = CompletableFuture.supplyAsync(() -> livecoinMarketplace.tradesByPair(pair), executorService)
-                .exceptionally(error -> {
-                    log.error("Marketplace error with {} pair", pair);
-                    return Collections.emptyList();
-                });
-        CompletableFuture<List<OptimalOrderDto>> firstCombined = exceptionally1.thenCombine(exceptionally2, this::combine);
-        CompletableFuture<List<OptimalOrderDto>> lastCombined = firstCombined.thenCombine(exceptionally3, this::combine);
 
-        List<OptimalOrderDto> result = getResult(lastCombined);
-        /*List<OptimalOrderDto> result = marketplaces.parallelStream()
-                .map(marketplace -> marketplace.tradesByPair(pair))
+        List<CompletableFuture<List<OptimalOrderDto>>> futures = marketplaces.stream().map(
+                m -> CompletableFuture.supplyAsync(() ->
+                m.tradesByPair(pair), executorService)
+                .applyToEither(timeoutAfter(TIMEOUT_SECONDS, TimeUnit.SECONDS), Function.identity())
+                .exceptionally(error -> {
+                    log.warn("Failed getOneSegmentDetails: " + error);
+                    return Collections.emptyList();
+                }))
+                .collect(Collectors.toList());
+
+        List<OptimalOrderDto> collect = futures.stream()
+                .map(CompletableFuture::join)
                 .flatMap(List::stream)
-                .collect(toList());*/
+                .collect(Collectors.toList());
+
+        executorService.shutdown();
+        return collect;
+    }
+
+    private  <T> CompletableFuture<T> timeoutAfter(long timeout, TimeUnit unit) {
+        ScheduledThreadPoolExecutor delayer = new ScheduledThreadPoolExecutor(1);
+
+        CompletableFuture<T> result = new CompletableFuture<>();
+        delayer.schedule(() -> result.completeExceptionally(new TimeoutException("Timeout after " + timeout)), timeout, unit);
         return result;
-    }
-
-    private List<OptimalOrderDto> getResult(CompletableFuture<List<OptimalOrderDto>> list) {
-        try {
-            return list.get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        return Collections.EMPTY_LIST;
-    }
-
-    private List<OptimalOrderDto> combine(List<OptimalOrderDto> first, List<OptimalOrderDto> second) {
-        List<OptimalOrderDto> resultList = new ArrayList<>(first);
-        resultList.addAll(second);
-        return resultList;
     }
 }
