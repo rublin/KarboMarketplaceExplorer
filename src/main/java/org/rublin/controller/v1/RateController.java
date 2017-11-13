abd4a472053fdd421b91590d401b9084024fe1f5
rublin <toor.ua@gmail.com>
2017-11-13 21:17:31 +0200

    add log timing to controller

--- src/main/java/org/rublin/controller/v1/RateController.java
+++ src/main/java/org/rublin/controller/v1/RateController.java
@@ -1,5 +1,6 @@
 package org.rublin.controller.v1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.rublin.dto.RateResponseDto;
 import org.rublin.service.RateService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = RateController.URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class RateController extends AbstractController {
@@ -19,6 +21,9 @@ public class RateController extends AbstractController {
 
     @RequestMapping(value = "/rate", method = RequestMethod.GET)
     public RateResponseDto rate() {
-        return rateService.getCurrentRate();
+        long start = System.currentTimeMillis();
+        RateResponseDto currentRate = rateService.getCurrentRate();
+        log.info("Current rate takes {} ms", System.currentTimeMillis() - start);
+        return currentRate;
     }
 }
