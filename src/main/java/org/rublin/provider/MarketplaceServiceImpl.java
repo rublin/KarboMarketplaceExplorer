a286176240718568227c9302ed67eb90c1c6e7cf
rublin <toor.ua@gmail.com>
2019-04-16 17:49:25 +0300

    disable providers from properties

--- src/main/java/org/rublin/provider/MarketplaceServiceImpl.java
+++ src/main/java/org/rublin/provider/MarketplaceServiceImpl.java
@@ -6,14 +6,21 @@ import org.rublin.dto.OrderResponseDto;
 import org.rublin.dto.PairDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -24,6 +31,28 @@ import static java.util.stream.Collectors.toList;
 public class MarketplaceServiceImpl implements MarketplaceService {
 
     private static final long TIMEOUT_SECONDS = 60;
+
+    @Value("${provider.btc-trade.enable}")
+    private boolean btcTradeEnabled;
+
+    @Value("${provider.crex.enable}")
+    private boolean crexEnabled;
+
+    @Value("${provider.cryptopia.enable}")
+    private boolean cryptopiaEnabled;
+
+    @Value("${provider.kuna.enable}")
+    private boolean kunaEnabled;
+
+    @Value("${provider.livecoin.enable}")
+    private boolean livecoinEnabled;
+
+    @Value("${provider.richamster.enable}")
+    private boolean richamsterEnabled;
+
+    @Value("${provider.tradeogre.enable}")
+    private boolean tradeogreEnabled;
+
     @Autowired
     @Qualifier("btc")
     private Marketplace btcTradeMarketplace;
@@ -61,11 +90,11 @@ public class MarketplaceServiceImpl implements MarketplaceService {
     public void createCache() {
         List<CompletableFuture<List<OrderResponseDto>>> futures = marketplaces.stream().map(
                 m -> CompletableFuture.supplyAsync(m::trades, executorService)
-                .applyToEither(timeoutAfter(TIMEOUT_SECONDS, TimeUnit.SECONDS), Function.identity())
-                .exceptionally(error -> {
-                    log.warn("Trades failed {}", error);
-                    return Collections.emptyList();
-                })
+                        .applyToEither(timeoutAfter(TIMEOUT_SECONDS, TimeUnit.SECONDS), Function.identity())
+                        .exceptionally(error -> {
+                            log.warn("Trades failed {}", error);
+                            return Collections.emptyList();
+                        })
         ).collect(toList());
 
         Map<TradePlatform, List<OrderResponseDto>> collect = futures.stream()
@@ -103,21 +132,29 @@ public class MarketplaceServiceImpl implements MarketplaceService {
         return orders;
     }
 
-    private  <T> CompletableFuture<T> timeoutAfter(long timeout, TimeUnit unit) {
-                CompletableFuture<T> result = new CompletableFuture<>();
+    private <T> CompletableFuture<T> timeoutAfter(long timeout, TimeUnit unit) {
+        CompletableFuture<T> result = new CompletableFuture<>();
         delayedThreadPool.schedule(() -> result.completeExceptionally(new TimeoutException("Timeout after " + timeout)), timeout, unit);
         return result;
     }
 
     @PostConstruct
     private void init() {
-        marketplaces = Arrays.asList(cryptopiaMarketplace,
-                livecoinMarketplace,
-                btcTradeMarketplace,
-                kunaMarketplace,
-                richamsterMarketplace,
-                tradeogreMarketplace,
-                crexMarketplace);
+        marketplaces = new ArrayList<>();
+        if (btcTradeEnabled)
+            marketplaces.add(btcTradeMarketplace);
+        if (crexEnabled)
+            marketplaces.add(crexMarketplace);
+        if (cryptopiaEnabled)
+            marketplaces.add(cryptopiaMarketplace);
+        if (kunaEnabled)
+            marketplaces.add(kunaMarketplace);
+        if (livecoinEnabled)
+            marketplaces.add(livecoinMarketplace);
+        if (richamsterEnabled)
+            marketplaces.add(richamsterMarketplace);
+        if (tradeogreEnabled)
+            marketplaces.add(tradeogreMarketplace);
 
         delayedThreadPool = new ScheduledThreadPoolExecutor(marketplaces.size());
         executorService = Executors.newFixedThreadPool(marketplaces.size(), r -> {
